f9f802696de31db9b9e07f89b9c5ba28e59fb703
creazy <mlodyv111@gmail.com>
2020-11-23 14:21:59 +0000

    empty

--- src/service/GroupedDailyScheduleService.jsx
+++ src/service/GroupedDailyScheduleService.jsx
@@ -1,6 +1,5 @@
 export const getGroupedDailySchedules = async (groupId) => {
     const host = "https://pk-schedule-api.herokuapp.com/";
-    const proxy = "http://localhost:8010/proxy/"
     const url = "schedules/latest/grouped-daily-schedules/group-id/";
     return await fetch(host + url + groupId)
         .then(res => res.json())
