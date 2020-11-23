aa2556413bdaf022dac0d7ed30e5b4b9ad4f4361
creazy <mlodyv111@gmail.com>
2020-11-23 19:27:56 +0000

    Added: script forcing ssl

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import HttpsRedirect from 'react-https-redirect';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
+  <HttpsRedirect>
   <React.StrictMode>
     <main>
       <App />
     </main>
-  </React.StrictMode>,
+  </React.StrictMode>
+  </HttpsRedirect>,
   document.getElementById('root')
 );
 
